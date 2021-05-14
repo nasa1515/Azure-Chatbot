96bb19f3d2410635d4082ca0c5ad1b4defb5d918
nasa1515 <h43254@gmail.com>
2021-05-14 17:19:01 +0900

    commit

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 8000
-    APP_ID = os.environ.get("MicrosoftAppId", "60e15acf-0a7b-4532-9b94-29c2eb1299ac")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", ";xeWO2AeCFm$$C!Gc+ota+|::z@>0K>")
+    APP_ID = os.environ.get("MicrosoftAppId", "e954c5dc-2691-4eb5-8882-e9c8663c96a5")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "L-1H2T69-MdMgPZa-.LtUO25m02l39.zb9")
