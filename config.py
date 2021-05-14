ba6dd1ef4da6298197e8f1cca8af924005c4bd1e
nasa1515 <h43254@gmail.com>
2021-05-14 16:18:55 +0900

    Commit

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ class DefaultConfig:
 
     PORT = 3978
     #APP_ID = os.environ.get("MicrosoftAppId", "60e15acf-0a7b-4532-9b94-29c2eb1299ac") --Service 추가한거
-    APP_ID = os.environ.get("MicrosoftAppId", "4b33c554-8c5d-4017-a915-2f67550fc89f")
+    APP_ID = os.environ.get("MicrosoftAppId", "cc484559-dab6-4b34-ba7e-4f1b6925297b")
     # 밑에는 Client Secret
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "lVzdd7W~BiW9I.sX2--5.vsCsx6-_09~3U")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "aU4c~Q3Ze87ACC.SGWh6j230~.AKfw84.Q")
