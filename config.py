8b7f2923a44e36bc2d5b5a8cdeb5ae39c3505172
nasa1515 <h43254@gmail.com>
2021-05-14 16:48:34 +0900

    Commit

--- config.py
+++ config.py
@@ -8,8 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    #APP_ID = os.environ.get("MicrosoftAppId", "60e15acf-0a7b-4532-9b94-29c2eb1299ac") --Service 추가한거
-    APP_ID = os.environ.get("MicrosoftAppId", "f3ade0ca-57c2-4b6f-9008-f4dabcc1ae36")
-    # 밑에는 Client Secret
-    
-    #APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "aU4c~Q3Ze87ACC.SGWh6j230~.AKfw84.Q")
+    APP_ID = os.environ.get("MicrosoftAppId", "60e15acf-0a7b-4532-9b94-29c2eb1299ac")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", ";xeWO2AeCFm$$C!Gc+ota+|::z@>0K>")
