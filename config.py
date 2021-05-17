4cf0db701c4f61d3eeb15800da35942510f6808e
nasa1515 <h43254@gmail.com>
2021-05-17 17:07:24 +0900

    Commit

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 8000
+    PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "0ccb5bb4-479b-41d9-8658-256b4281b390")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "wtBUn*k5#Y=zusp+)20a33q^_sE")
